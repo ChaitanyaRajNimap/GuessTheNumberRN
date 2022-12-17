678e644c930536b1ed5dca4d1cb625d7b091671a
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2022-12-17 13:01:24 +0530

    Button on start game screen improved

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,60 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
-      BUTTONS
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.button}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.button}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    borderRadius: 10,
+    marginTop: 100,
+    marginHorizontal: 24,
+    alignItems: "center",
+    backgroundColor: "#4e0329",
+    elevation: 10, //Like box-shadow only for Android
+    justifyContent: "center",
+    shadowColor: "black", // Like boax-shadow for IOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    marginVertical: 10,
+    color: "#ddb52f",
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  button: {
+    flex: 1,
+  },
+});
